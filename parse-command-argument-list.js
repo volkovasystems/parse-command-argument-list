661a2dea0717096d7fb47db78c1adbdc36270a78
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-09-24 21:36:01 +0800

    Updated to hard tabs.

--- parse-command-argument-list.js
+++ parse-command-argument-list.js
@@ -1,84 +1,84 @@
 /*:
-    @module-license:
-        The MIT License (MIT)
+	@module-license:
+		The MIT License (MIT)
 
-        Copyright (c) 2014 Richeve Siodina Bebedor
+		Copyright (c) 2014 Richeve Siodina Bebedor
 
-        Permission is hereby granted, free of charge, to any person obtaining a copy
-        of this software and associated documentation files (the "Software"), to deal
-        in the Software without restriction, including without limitation the rights
-        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions:
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
 
-        The above copyright notice and this permission notice shall be included in all
-        copies or substantial portions of the Software.
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
 
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
-    @end-module-license
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-module-license
 
-    @module-configuration:
-        {
-            "packageName": "parse-command-argument-list",
-            "fileName": "parse-command-argument-list.js",
-            "moduleName": "parseCommandArgumentList",
-            "authorName": "Richeve S. Bebedor",
-            "authorEMail": "richeve.bebedor@gmail.com",
-            "repository": "git@github.com/volkovasystems/parse-command-argument-list.git",
-            "isGlobal": "true"
-        }
-    @end-module-configuration
+	@module-configuration:
+		{
+			"packageName": "parse-command-argument-list",
+			"fileName": "parse-command-argument-list.js",
+			"moduleName": "parseCommandArgumentList",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com/volkovasystems/parse-command-argument-list.git",
+			"isGlobal": "true"
+		}
+	@end-module-configuration
 
-    @module-documentation:
+	@module-documentation:
 
-    @end-module-documentation
+	@end-module-documentation
 
-    @include:
-        {
-            "camelize-namespace.js@github.com/volkovasystems": "camelizeNamespace"
-        }
-    @end-include
+	@include:
+		{
+			"camelize-namespace.js@github.com/volkovasystems": "camelizeNamespace"
+		}
+	@end-include
 */
 var parseCommandArgumentList = function parseCommandArgumentList( ){
-    var commandArgumentList = process.argv.slice( 2 );
+	var commandArgumentList = process.argv.slice( 2 );
 
-    var argumentValue = "";
-    var argumentSet = { };
+	var argumentValue = "";
+	var argumentSet = { };
 
-    var commandArgumentListLength = commandArgumentList.length;
-    for( var index = 0; index < commandArgumentListLength; index++ ){
-        argumentValue = commandArgumentList[ index ];
+	var commandArgumentListLength = commandArgumentList.length;
+	for( var index = 0; index < commandArgumentListLength; index++ ){
+		argumentValue = commandArgumentList[ index ];
 
-        var argumentValueList = argumentValue.split( "=" );
-        if( argumentValueList.length == 2 ){
-            var key = argumentValueList[ 0 ];
-            var value = argumentValueList[ 1 ];
+		var argumentValueList = argumentValue.split( "=" );
+		if( argumentValueList.length == 2 ){
+			var key = argumentValueList[ 0 ];
+			var value = argumentValueList[ 1 ];
 
-            key = key.replace( DASHED_COMMAND_PATTERN, "" );
-            key = camelizeNamespace( key );
+			key = key.replace( DASHED_COMMAND_PATTERN, "" );
+			key = camelizeNamespace( key );
 
-            argumentSet[ key ] = value;
+			argumentSet[ key ] = value;
 
-        }else if( DASHED_COMMAND_PATTERN.test( argumentValue ) ){
-            var key = argumentValue;
+		}else if( DASHED_COMMAND_PATTERN.test( argumentValue ) ){
+			var key = argumentValue;
 
-            key = key.replace( DASHED_COMMAND_PATTERN, "" );
-            key = camelizeNamespace( key );
+			key = key.replace( DASHED_COMMAND_PATTERN, "" );
+			key = camelizeNamespace( key );
 
-            argumentSet[ key ] = true;
+			argumentSet[ key ] = true;
 
-        }else{
-            argumentSet[ index ] = argumentValue;
-        }
-    }
+		}else{
+			argumentSet[ index ] = argumentValue;
+		}
+	}
 
-    return argumentSet;
+	return argumentSet;
 };
 
 const DASHED_COMMAND_PATTERN = /^\-+/;
